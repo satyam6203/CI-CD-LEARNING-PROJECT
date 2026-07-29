e0be954453dbe8f678515f9a8efc340e289102ce
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-07-29 23:39:29 +0530

    files added

---
+++ src/main/java/com/cicd/cicdDemo/Controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.cicd.cicdDemo.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/satyam")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok().body("hello my name is satyam kumar singh");
+    }
+
+    @GetMapping("/about")
+    public ResponseEntity<String> about(){
+        return ResponseEntity.ok().body("i am currently persuing BTech in parul university");
+
+    }
+}
