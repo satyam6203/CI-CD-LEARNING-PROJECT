59cc778c9761609778686d8106937f1c359e40e0
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-08-13 11:58:17 +0530

    new ulr for the experience

--- src/main/java/com/cicd/cicdDemo/Controller/HomeController.java
+++ src/main/java/com/cicd/cicdDemo/Controller/HomeController.java
@@ -39,4 +39,9 @@ public class HomeController {
     public String project(){
         return "i am currently working on airline project";
     }
+    
+    @GetMapping("/exp")
+    public String Experience(){
+        return "i am currently working on nyric.ai";
+    }
 }
